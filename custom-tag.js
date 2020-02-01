fc1ba0848dabe6dc2647721175b9487fe96cf189
RoshanShrestha123 <roshan.shrestha4u@gmail.com>
2020-02-01 11:24:52 +0545

    template added in the component

--- custom-tag.js
+++ custom-tag.js
@@ -1,8 +1,12 @@
+const template = document.createElement('template');
+template.innerHTML = `<style>h2{color:blue}</style><h2></h2>`;
+
 class CustomTag extends HTMLElement{
     constructor(){
         super();
         this.attachShadow({mode:'open'});
-        this.shadowRoot.innerHTML = `<style>h2{color:blue}</style><h2>${this.getAttribute('name')}</h2>`;
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector('h2').innerHTML = this.getAttribute('name');
     }
 }
 window.customElements.define('custom-tag',CustomTag);
